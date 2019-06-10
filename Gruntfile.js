95def61adf0cd7c942ac6eb871f74a796a305b47
Sam Griffin <sam.griffin@verint.com>
2019-06-10 15:05:52 +0100

    Adding file structure

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,3 +1,26 @@
 module.exports = function(grunt) {
+
+    grunt.initConfig({
+        jasmine: {
+            coverage: {
+                src: ['src/main/js/*.js'],
+                options: {
+                    specs: ['src/test/js/*.js'],
+                    template: require('grunt-template-jasmine-istanbul'),
+                    templateOptions: {
+                        coverage: 'bin/coverage/coverage.json',
+                        report: 'bin/coverage',
+                        thresholds: {
+                            lines: 75,
+                            statements: 75,
+                            branches: 75,
+                            functions: 90
+                        }
+                    }
+                }
+            }
+        }
+    }
+
     grunt.loadNpmTasks('grunt-contrib-jasmine');
 };
---
+++ src/main/js/Main.js
@@ -0,0 +1,3 @@
+var v = 1;
+v += 2;
+console.log(v);
