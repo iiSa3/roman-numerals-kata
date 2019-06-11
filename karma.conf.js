ab0e54bbf24e77f71b57064d48a4616cf180d746
Sam Griffin <sam.griffin@verint.com>
2019-06-11 14:21:24 +0100

    Adding jasmine report

--- karma.conf.js
+++ karma.conf.js
@@ -1,16 +1,19 @@
 // karma.conf.js
 module.exports = function(config) {
     config.set({
-        basePath: 'src',
-        files: ['main/js/**/*.js', 'test/js/*Spec.js'],
+        reporters: ['progress', 'html'],
+        htmlReporter: {
+            outputFile: 'tests/jasmine.html',
+            pageTitle: 'Jasmine Tests'
+        },
+        basePath: '.',
+        files: ['src/main/js/**/*.js', 'src/test/js/*Spec.js'],
         frameworks: ['jasmine'],
         browsers: ['Chrome'],
         reporters: ['dots'],
         port: 9876,
         colors: true,
         logLevel: config.LOG_INFO,
-        autoWatch: true,
-        // browsers: ['PhantomJS'],
-        // singleRun: true
+        autoWatch: true
     });
 };
